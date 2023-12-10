4e8d7f80a8833678f02441785c4e9030f817d3c2
Anton Savitskiy <sygeman@gmail.com>
2023-12-10 21:53:48 +0300

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -4,7 +4,7 @@
 // @version      2023-12-10
 // @description  try to take over the world!
 // @author       You
-// @match        https://music.yandex.ru/home
+// @match        https://music.yandex.ru/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=yandex.ru
 // @grant        none
 // ==/UserScript==
